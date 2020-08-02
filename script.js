8189fbb6d938515beaa6fe5bb3102cf751486d15
tobymould <toby.mould@icloud.com>
2020-08-03 00:05:12 +0100

    fixed the ability to display the user guess on the page by implementing 2 separate div's for the guessed number dispay and the result on how close they were

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const msgEl = document.getElementById('msg');
+const msgEl2 = document.getElementById('msg2');
 const start = document.querySelector('#start-button');
 // const stop = document.querySelector('#stop-button');
 
@@ -37,34 +38,34 @@ console.log('Number:', randomNum);
 
 const checkNumber = msg => {
   if (Number.isNaN(msg)) {
-    msgEl.innerHTML = `<div>Your guess was not a number. Please try again.</div>`;
+    msgEl2.innerHTML = `<div>Your guess was not a number. Please try again.</div>`;
   } else if (randomNum === msg) {
-    msgEl.innerHTML = `<div>yaay, you guessed correctly =D</div>`;
+    msgEl2.innerHTML = `<div>CORRECT! YAAAAAY! You guessed correctly =D</div>`;
   } else if (randomNum < msg) {
-    msgEl.innerHTML = `<div>TOO HIGH</div>`;
+    msgEl2.innerHTML = `<div>TOO HIGH</div>`;
   } else if (randomNum > msg) {
-    msgEl.innerHTML = `<div>TOO LOW</div>`;
+    msgEl2.innerHTML = `<div>TOO LOW</div>`;
   }
 
   const toleranceValue = Math.abs(randomNum - msg);
   console.log(msg);
   console.log(toleranceValue);
   if (msg > 100 || msg < 1) {
-    msgEl.innerHTML += `<div>You need to guess a number between 1 - 100 only</div>`;
+    msgEl2.innerHTML += `<div>You need to guess a number between 1 - 100 only</div>`;
   } else if (toleranceValue >= 60 && toleranceValue < 100) {
-    msgEl.innerHTML += `<div>you are guessing on the complete opposite end of the range. Try again :)</div>`;
+    msgEl2.innerHTML += `<div>You are guessing on the complete opposite end of the range. Try again :)</div>`;
   } else if (toleranceValue >= 40 && toleranceValue < 60) {
-    msgEl.innerHTML += `<div>You are frozen cold you're so far</div>`;
+    msgEl2.innerHTML += `<div>You are frozen cold you're so far</div>`;
   } else if (toleranceValue >= 20 && toleranceValue < 40) {
-    msgEl.innerHTML += `<div>You are cold</div>`;
+    msgEl2.innerHTML += `<div>You are cold</div>`;
   } else if (toleranceValue >= 10 && toleranceValue < 20) {
-    msgEl.innerHTML += `<div>You are warm</div>`;
+    msgEl2.innerHTML += `<div>You are warm</div>`;
   } else if (toleranceValue >= 5 && toleranceValue < 10) {
-    msgEl.innerHTML += `<div>You are hot!</div>`;
+    msgEl2.innerHTML += `<div>You are hot!</div>`;
   } else if (toleranceValue >= 3 && toleranceValue < 5) {
-    msgEl.innerHTML += `<div>You are boiling you're sooooo close!!</div>`;
+    msgEl2.innerHTML += `<div>You are boiling you're sooooo close!!</div>`;
   } else if (toleranceValue >= 1 && toleranceValue < 3) {
-    msgEl.innerHTML += `<div>You couldn't be closer if you tried!</div>`;
+    msgEl2.innerHTML += `<div>You couldn't be closer if you tried!</div>`;
   }
   return;
 };
@@ -72,4 +73,9 @@ const checkNumber = msg => {
 // speak result:
 recognition.addEventListener('result', onSpeak);
 start.addEventListener('click', startListening);
+document.body.addEventListener('click', event => {
+  if (event.target.id === 'play-again') {
+    window.location.reload();
+  }
+});
 // stop.addEventListener('click', stopListening);
